93d794c9515995b435dac659f3de289518a9eeaf
wanyuran <wan_ivy@163.com>
2020-03-24 15:45:49 +0800

    refactor method getCurrentQuality for diffrent product type

--- src/main/java/cn/xpbootcamp/gilded_rose/Product.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Product.java
@@ -1,14 +1,23 @@
 package cn.xpbootcamp.gilded_rose;
 
 public class Product {
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
+
     private int sellIn;
     private int initialQuality;
     private int depreciationPerDay;
+    private String type;
 
-    public  Product(int sellIn, int initialQuality, int depreciationPerDay){
+    public Product(int sellIn, int initialQuality, int depreciationPerDay) {
         this.sellIn = sellIn;
         this.initialQuality = initialQuality;
         this.depreciationPerDay = depreciationPerDay;
+    }
+
+    public Product(int sellIn, int initialQuality, int depreciationPerDay, String type) {
+        this(sellIn, initialQuality, depreciationPerDay);
+        this.type = type;
     }
 
     public int getSellIn() {
@@ -35,8 +44,29 @@ public class Product {
         this.depreciationPerDay = depreciationPerDay;
     }
 
-    public int getCurrentQuality(int passedDays) {
-        int currentQuality = initialQuality - passedDays * depreciationPerDay;
-        return Math.max(currentQuality,0);
+    public int getCurrentQuality(int passedDays, String type) {
+        int remainingDays = sellIn - passedDays;
+
+        if ("Aged_Brie".equals(type) || "Backstage pass".equals(type)) {
+            if (remainingDays > 10) {
+                int currentQuality = initialQuality;
+                return currentQuality;
+            } else if (remainingDays > 5 && remainingDays <= 10) {
+                int currentQuality = Math.max(50, initialQuality + 2 * (remainingDays - 5));
+                return currentQuality;
+            } else if (remainingDays >= 0 && remainingDays <= 5) {
+                int currentQuality = Math.max(50, initialQuality + 3 * remainingDays + 20);
+                return currentQuality;
+            } else if (remainingDays < 0) {
+                int currentQuality = 0;
+                return currentQuality;
+            }
+        } else if ("Sulfuras".equals(type)) {
+            int currentQuality = initialQuality;
+            return currentQuality;
+        }
+
+        int currentQuality = Math.min(0, initialQuality - remainingDays * depreciationPerDay);
+        return currentQuality;
     }
 }
